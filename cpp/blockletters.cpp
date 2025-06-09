ffe5b0b885d599652b957d57df68174af821d83a
aubreyjcook <aubreyjcook@gmail.com>
2025-06-08 21:24:55 -0500

    update

--- cpp/blockletters.cpp
+++ cpp/blockletters.cpp
@@ -2,6 +2,11 @@
 
 int main() {
   // I can write C++
-  std::cout << "  A     CCC n\";  
-
+  std::cout << "  A     CCC \n";
+  std::cout << " A A   C   C\n";
+  std::cout << "A   A  C    \n";
+  std::cout << "AAAAA  C    \n";
+  std::cout << "A   A  C    \n";
+  std::cout << "A   A  C   C\n";
+  std::cout << "A   A   CCC \n";
 }
